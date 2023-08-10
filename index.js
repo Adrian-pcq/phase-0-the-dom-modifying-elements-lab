f2e0c87791ad277255ab5c09113a811c3ad94072
Adrian <adrianqueizan@gmail.com>
2023-08-10 10:17:00 -0400

    creates domNode

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const del = document.getElementById("main");
+del.remove();
+
+const newHeader = document.createElement("h1");
+document.body.append(newHeader);
+
+newHeader.setAttribute('id', 'victory');
+
+const text = document.createTextNode("Adrian is the champion");
+newHeader.appendChild(text);
